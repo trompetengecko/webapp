bc556bbba68bfa0be1679dbf61b6c15252aa0f1c
Samuel Klippert <samuel.klippert@gls-germany.com>
2022-05-20 20:47:21 +0200

    completed Nr 5 from week 4

---
+++ Übungen/4/5.js
@@ -0,0 +1,6 @@
+function calcBMI() {
+    let weight = document.getElementById("weight").value
+    let height = document.getElementById("height").value
+    document.getElementById("BMI-Output").innerHTML = weight / Math.pow(height, 2);
+    //window.alert(weight / Math.pow(height, 2));
+}
