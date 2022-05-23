bfb7330471d667945767e46de4f26785b67fe2a6
Samuel Klippert <samuel.klippert@informatik.hs-fulda.de>
2022-05-23 16:08:16 +0200

    tried to cleanup the onclick

--- Übungen/4/5.js
+++ Übungen/4/5.js
@@ -1,6 +1,7 @@
-document.getElementById("BMI-Button").onclick = calcBMI;
+//document.getElementById("BMI-Button").onclick = calcBMI;
+document.getElementById("BMI-Button").addEventListener("click", function() {
 
-function calcBMI() {
+//function calcBMI() {
     let weight = document.getElementById("weight").value;
     let height = document.getElementById("height").value;
     document.getElementById("BMI-Output").innerHTML = weight / Math.pow(height, 2);
