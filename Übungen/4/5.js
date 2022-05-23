5504fde1703fce06c3f4933acbbb99d52490e02b
Samuel Klippert <samuel.klippert@informatik.hs-fulda.de>
2022-05-23 16:20:50 +0200

    Replaced onclick() function with JS eventlistener

--- Übungen/4/5.js
+++ Übungen/4/5.js
@@ -1,9 +1,7 @@
 //document.getElementById("BMI-Button").onclick = calcBMI;
-document.getElementById("BMI-Button").addEventListener("click", function() {
-
-//function calcBMI() {
+const button = document.getElementById("BMI-Button");
+button.addEventListener("click", function() {
     let weight = document.getElementById("weight").value;
     let height = document.getElementById("height").value;
     document.getElementById("BMI-Output").innerHTML = weight / Math.pow(height, 2);
-    //window.alert(weight / Math.pow(height, 2));
-}
+},false)
